98eca2bb36d0bc2e867aae0329dd2dba3f878b3c
baibaoqing <ilahsa@126.com>
2018-08-13 17:58:20 +0800

    update

--- src/main/java/com/alta/service/TextFilter.java
+++ src/main/java/com/alta/service/TextFilter.java
@@ -7,6 +7,21 @@ public class TextFilter {
     private Map<Object,Object> sensitiveWordsMap;
     private static final String END_FLAG="end";
 
+    public static void main(String[] args) {
+        Set<String> sensitiveWords=new HashSet<>();
+        sensitiveWords.add("你是傻逼");
+        sensitiveWords.add("你是傻逼啊");
+        sensitiveWords.add("你是坏蛋");
+        sensitiveWords.add("你个大笨蛋");
+        sensitiveWords.add("我去年买了个表");
+        sensitiveWords.add("shit");
+
+        TextFilter textFilter=new TextFilter();
+        textFilter.initSensitiveWordsMap(sensitiveWords);
+        String text="你你你你是傻逼啊你，说你呢，你个大笨蛋。";
+        System.out.println(textFilter.getSensitiveWords(text,MatchType.MAX_MATCH));
+    }
+    
     /**
      * 初始化敏感词过滤的map
      * @param sensitiveWords 敏感词列表
